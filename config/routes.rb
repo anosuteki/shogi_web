4249c01f16a6ffcb8356b13538d30d56548507e9
akicho8 <akicho8@gmail.com>
2020-05-02 11:15:57 +0900

    [from now] 2020/05/02 11:15:57

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,7 @@
 Rails.application.routes.draw do
-  resources :foo_articles
-  resources :about, only: :show
-
-  get "health" => HealthResponder
-
-  get "talk", to: "talk#show", as: :talk
-
   devise_for :xusers, {
     class_name: "Colosseum::User",
   }
-
-  ################################################################################ 対戦
-
-  namespace :colosseum do
-    resources :battles
-    resources :users
-    resources :rankings, only: :index
-  end
 
   # root "colosseum/battles#index"
   root "admin/scripts#show", { id: :acns1_sample }
@@ -27,106 +12,9 @@ Rails.application.routes.draw do
 
   ################################################################################ ログアウト
 
-  namespace :colosseum, path: "" do
-    resource :session, only: [:create, :destroy]
-  end
-
-  ################################################################################ 将棋ウォーズ棋譜検索
-
-  namespace :swars, path: "" do
-    resources :battles, path: "w"
-    resources :player_infos, :only => :index, path: "w-user-stat"
-
-    get "wr/:id",   to: "battles#show" # 互換性のため
-
-    get "w",        to: "battles#index", as: :basic
-    get "w-light",  to: "battles#index", as: :light
-  end
 
   resolve "Swars::User" do |user, options|
     swars_basic_path(query: user.to_param)
-  end
-
-  ################################################################################ 棋譜投稿
-
-  resources :free_battles, path: "x"
-
-  # resolve "FreeBattle" do |record, options|
-  #   [record, options]
-  # end
-
-  # resolve "FreeBattle" do |record, options|
-  #   [record, options]
-  # end
-
-  get "adapter", to: "free_battles#new", defaults: { edit_mode: "adapter" }
-
-  # resolve "FreeBattle" do |free_battle, options|
-  #   [ns_prefix, free_battle, options]
-  # end
-
-  ################################################################################ 戦法トリガー事典
-
-  resources :tactic_notes, path: "tactics", only: [:index, :show]
-
-  get "tactics-tree",    to: "tactic_notes#index", defaults: {mode: "tree"},    as: :tree
-
-  ################################################################################ 符号の鬼
-
-  resource :stopwatch, only: [:show, :create]
-  resource :simple_board, path: "board", only: [:show, :create]
-  resource :share_board, path: "share-board", only: [:show, :create]
-
-  resources :xy_records, path: "xy", only: [:index, :create, :update]
-
-  ################################################################################ 局面編集
-
-  get "position-editor", to: "position_editor#show", as: :position_editor
-
-  ################################################################################ CPU対戦
-
-  resource :cpu_battles, path: "cpu/battles", only: [:show, :create]
-  get "cpu/battles", to: "cpu_battles#show"
-
-  ################################################################################ 外部リンク
-
-  direct :official_swars_battle do |battle, options = {}|
-    "https://shogiwars.heroz.jp/games/#{battle.key}"
-  end
-
-  direct :swars_home do |user, options = {}|
-    "https://shogiwars.heroz.jp/users/mypage/#{user.user_key}"
-  end
-
-  direct :piyo_shogi_app do |url, options|
-    # if Rails.env.development?
-    #   url = "http://wdoor.c.u-tokyo.ac.jp/shogi/LATEST//2016/09/24/wdoor+floodgate-600-10F+gpsfish_normal_1c+gps_l+20160924113005.csa"
-    # end
-    "piyoshogi://?#{options.to_query}&url=#{url}" # 渡すURLをエスケープするとぴよ将棋で読めなくなるので to_query してはいけない
-  end
-
-  direct :kento_app do |options, turn|
-    "https://www.kento-shogi.com/?#{options.to_query}##{turn}"
-  end
-
-  direct :google_search do |query|
-    "https://www.google.co.jp/search?q=#{query}"
-  end
-
-  direct :google_image_search do |query|
-    "https://www.google.co.jp/search?tbm=isch&q=#{query}"
-  end
-
-  direct :twitter_search do |query|
-    "https://twitter.com/search?q=#{query}"
-  end
-
-  direct :google_maps do |query|
-    "https://www.google.co.jp/maps/search/#{query}"
-  end
-
-  direct :youtube_search do |query|
-    "https://www.youtube.com/results?search_query=#{query}&search=Search"
   end
 
   direct :production_app do
@@ -135,18 +23,6 @@ Rails.application.routes.draw do
 
   direct :staging_app do
     "https://staging.shogi-extend.com/"
-  end
-
-  ################################################################################ scripts
-
-  resources :scripts, :path => "script", :only => [:show, :update]
-
-  ################################################################################ api
-
-  namespace :api, format: "json" do
-    resource :general, only: [:show] do
-      match "any_source_to", via: [:get, :post]
-    end
   end
 
   ################################################################################ admin
@@ -160,18 +36,5 @@ Rails.application.routes.draw do
     resources :example_scripts, path: "example-script", only: [:show, :update]
 
     root "homes#show"
-
-    ################################################################################ fastentry
-
-    if true
-      require "fastentry/engine"
-
-      if Rails.env.production? || Rails.env.staging?
-        Fastentry::ApplicationController.http_basic_authenticate_with name: "admin", password: Rails.application.credentials[:admin_password]
-      end
-
-      # http://localhost:3000/admin/rails-cache
-      mount Fastentry::Engine, at: "rails-cache"
-    end
   end
 end
